a4690a3692bac7480c6c3a71a1dad005cd33cbc1
Jens Pfahl <jens.pfahl@mailbox.org>
2024-02-25 15:30:53 +0100

    functions for crypto operations

--- popup/enter_ip.js
+++ popup/enter_ip.js
@@ -21,15 +21,9 @@ document.addEventListener("click", (e) => {
   }
 });
 
-let uuid = crypto.randomUUID();
-console.log(uuid);
-const array = new Uint32Array(10);
-crypto.getRandomValues(array);
+let webClientId = crypto.randomUUID();
+console.log("webClientId = " + webClientId);
 
-console.log("Your lucky numbers:");
-for (const num of array) {
-  console.log(num);
-}
 
 let keyPair = window.crypto.subtle.generateKey(
   {
@@ -40,9 +34,118 @@ let keyPair = window.crypto.subtle.generateKey(
   },
   true,
   ["encrypt", "decrypt"],
-).then(keyPair => {
-  console.log(keyPair);
+).then(async keyPair => {
+  console.log("RSA Key = " + JSON.stringify(keyPair));
+
+  const sessionKey = await generateSessionKey();
+  const sessionKeyAsArray = await sessionKeyToArray(sessionKey);
+  console.log("AES Session Key = " + bytesToBase64(sessionKeyAsArray));
+
+  const encryptedMessage = await encryptMessage(sessionKey, "secret");
+  console.log("Encrypted message = " + encryptedMessage);
+
+  const encSessionKey = await encryptWithPublicKey(keyPair.publicKey, sessionKeyAsArray);
+  console.log("Encrypted AES Session Key = " + bytesToBase64(encSessionKey));
+
+
+  const decSessionKeyAsArray = await decryptWithPrivateKey(keyPair.privateKey, encSessionKey);
+  console.log("Decrypted AES Session Key = " + bytesToBase64(decSessionKeyAsArray));
+
+  const decSessionKey = await arrayToSessionKey(decSessionKeyAsArray);
+  const decryptedMessage = await decryptMessage(decSessionKey, encryptedMessage);
+  console.log("Decrypted message = " + decryptedMessage);
+  
 });
+
+
+function generateSessionKey() {
+  return window.crypto.subtle.generateKey(
+    {
+      name: "AES-GCM",
+      length: 128,
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+async function sessionKeyToArray(sessionKey) {
+  const exported = await window.crypto.subtle.exportKey("raw", sessionKey);
+  return new Uint8Array(exported); 
+}
+
+async function arrayToSessionKey(array) {
+  return window.crypto.subtle.importKey("raw", array, 
+  "AES-GCM", true, [
+    "encrypt",
+    "decrypt",
+  ]);
+}
+
+async function encryptMessage(sessionKey, message) {
+  const enc = new TextEncoder();
+  const encoded = enc.encode(message); 
+  const iv = window.crypto.getRandomValues(new Uint8Array(12));
+  const ciphertext = await window.crypto.subtle.encrypt(
+    { name: "AES-GCM", iv: iv },
+    sessionKey,
+    encoded,
+  );
+
+  console.log(iv);
+  console.log(new Uint8Array(ciphertext));
+
+  return bytesToBase64(iv) + ":" + bytesToBase64(new Uint8Array(ciphertext));
+}
+
+async function decryptMessage(sessionKey, encrypted) {
+  const splitted = encrypted.split(":");
+  const iv = base64ToBytes(splitted[0]);
+  const ciphertext = base64ToBytes(splitted[1]);
+  console.log(iv);
+  console.log(ciphertext);
+  const decrypted = await window.crypto.subtle.decrypt(
+    { name: "AES-GCM", iv }, 
+    sessionKey, 
+    ciphertext
+  );
+  return new TextDecoder().decode(new Uint8Array(decrypted));
+}
+
+
+async function encryptWithPublicKey(publicKey, payload) {
+  const rsaEncrypted = await window.crypto.subtle.encrypt(
+    {
+      name: "RSA-OAEP",
+    },
+    publicKey,
+    payload,
+  );
+
+  return new Uint8Array(rsaEncrypted);
+}
+
+async function decryptWithPrivateKey(privateKey, encrypted) {
+  const rsaDecrypted = await window.crypto.subtle.decrypt(
+    {
+      name: "RSA-OAEP",
+    },
+    privateKey,
+    encrypted,
+  );
+
+  return new Uint8Array(rsaDecrypted);
+}
+
+function base64ToBytes(base64) {
+  const binString = atob(base64);
+  return Uint8Array.from(binString, (m) => m.codePointAt(0));
+}
+
+function bytesToBase64(bytes) {
+  const binString = String.fromCodePoint(...bytes);
+  return btoa(binString);
+}
 
 
 
